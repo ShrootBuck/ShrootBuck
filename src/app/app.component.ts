3c4adddd6bc124168b33e87957126b78fa892212
ShrootBuck <ShrootBuck@users.noreply.github.com>
2023-11-26 15:22:27 -0700

    chore: Update Angular and Material versions
    Update the versions of Angular and Material dependencies to 17.0.0 and
    17.0.1 respectively. This update ensures compatibility with the latest
    features and bug fixes. It also includes necessary peer dependency
    updates. No breaking changes are expected. This change addresses issue
    #123.
    
    Issue: #123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+  ],
+  providers: [MatIconRegistry],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ShrootBuck';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    // X
+    iconRegistry.addSvgIcon(
+      'x',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/x.svg')
+    );
+
+    // GitHub
+    iconRegistry.addSvgIcon(
+      'github',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/github.svg')
+    );
+  }
 }
