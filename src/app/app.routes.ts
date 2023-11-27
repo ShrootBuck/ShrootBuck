85168200876b73908ca28ad88e174669af30e0a5
ShrootBuck <ShrootBuck@users.noreply.github.com>
2023-11-27 14:47:30 -0700

    Refactor styles and routes for ShrootBuck app
    Improve styling and routing for a more visually appealing and functional
    app experience. Updates include adding a custom theme, applying a
    prebuilt theme from Angular Material, and enhancing the layout and
    appearance of the toolbar. Also modifies the app routes to include an
    index page and a not-found page. These changes enhance the user
    interface and provide a more coherent and user-friendly navigation
    structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: IndexComponent },
+
+  // Leave 404 page at the end
+  { path: '**', component: NotFoundComponent },
+];
