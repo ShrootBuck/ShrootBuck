d1e57f1bf0a151be484196c8b50b050108daaf62
ShrootBuck <ShrootBuck@users.noreply.github.com>
2023-12-05 19:52:15 -0700

    Adjust default port to 8080 for improved compatibility
    This change updates the default port for running the application to 8080
    instead of 8000. This adjustment enhances compatibility with common
    deployment environments and improves the chances of successful
    application execution. Issue #123.

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def notFound(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
