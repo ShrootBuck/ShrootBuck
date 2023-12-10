92c0e0ec572196ecf8659bfc1dee32c07e579a8a
ShrootBuck <ShrootBuck@users.noreply.github.com>
2023-12-10 12:11:19 -0700

    Add caching headers to server responses
    This change adds caching headers to server responses in order to prevent
    caching of the content. The Cache-Control, Pragma, and Expires headers
    are set to ensure that the browser always retrieves the latest version
    of the content. This helps to avoid displaying outdated or cached data
    to the user.
    
    Issue: N/A

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def notFound(error):
     return flask.send_from_directory("dist/shroot-buck/browser", "index.html")
 
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
