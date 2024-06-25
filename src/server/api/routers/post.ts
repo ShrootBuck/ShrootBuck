4545538d1880dbc230d97dccdd0d2a8523d70ae2
ShrootBuck <ShrootBuck@users.noreply.github.com>
2024-06-25 19:04:47 +0300

    AWS deployment testing = bad commit messages

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -28,7 +28,7 @@ export const postRouter = createTRPCRouter({
       //   },
       // });
     }),
-
+ 
   getLatest: protectedProcedure.query(({ ctx }) => {
     // return ctx.db.post.findFirst({
     //   orderBy: { createdAt: "desc" },
