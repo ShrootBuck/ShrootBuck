48e330ff471e96e2dbcaf351eb4bab0af10a434d
ShrootBuck <ShrootBuck@users.noreply.github.com>
2024-12-30 15:54:18 -0700

    Try to fix SEO description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { ThemeProvider } from "~/components/theme-provider";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Zayd Krunz",
@@ -18,6 +17,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="A programmer from Tucson, AZ. I like to make things."
+        />
       </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
