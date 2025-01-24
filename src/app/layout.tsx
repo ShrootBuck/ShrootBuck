aca160e31422474573c150cdd96b62d7342a4c49
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-01-24 16:01:29 -0700

    Add programming languages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,24 @@ import { SocialsDock } from "./socials-dock";
 import Link from "next/link";
 import { LucideMapPin } from "lucide-react";
 import { prisma } from "~/lib/utils";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
+
+type Language = {
+  name: string; // Used for SVGs
+};
+
+const languages: Language[] = [
+  { name: "go" },
+  { name: "zig" },
+  { name: "javascript" },
+  { name: "lua" },
+  { name: "php" },
+  { name: "python" },
+  { name: "rust" },
+  { name: "typescript" },
+].sort((a, b) => a.name.localeCompare(b.name));
 
 export default async function Home() {
   const currentLocation = await prisma.location.findFirst({
@@ -20,7 +36,6 @@ export default async function Home() {
         <TextHoverEffect text="Zayd Krunz" />
       </h1>
       <h2 className="text-center text-xl">A programmer from Tucson, AZ</h2>
-
       <h3 className="flex items-center justify-center gap-2 pt-12 text-center text-lg leading-none">
         <LucideMapPin className="text-blue-600" />
         <span className="whitespace-normal">
@@ -28,15 +43,40 @@ export default async function Home() {
           <span>{currentLocation?.location}</span>
         </span>
       </h3>
-
       <p className="text-center">
         <Link className="text-blue-600 hover:text-blue-500" href="/location">
           How does this work?
         </Link>
       </p>
+      <h3 className="pt-12 text-center text-lg">Programming Languages</h3>
+      <div className="flex flex-wrap justify-center">
+        {languages.map((lang) => (
+          <Image
+            key={lang.name}
+            src={`/${lang.name}.svg`}
+            alt={lang.name}
+            width={48}
+            height={48}
+            className="m-2"
+          />
+        ))}
+      </div>
+      <div className="flex flex-col items-center justify-center p-10 md:flex-row">
+        <div className="flex flex-col items-center md:flex-row">
+          <img
+            src="https://github-readme-stats.vercel.app/api?username=ShrootBuck&hide=stars&hide_title=false&hide_rank=true&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=dark&locale=en&hide_border=false&order=1"
+            alt="stats graph"
+            className="mb-5 md:mb-0 md:mr-5"
+          />
+          <img
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=ShrootBuck&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=dark&hide_border=false&order=2"
+            alt="languages graph"
+          />
+        </div>
+      </div>
 
-      <h3 className="pt-12 text-center text-lg">Other Cool Stuff</h3>
-      <div className="flex justify-center">
+      <h3 className="text-center text-lg">Other Things</h3>
+      <div className="flex justify-center pb-5">
         <ul>
           <li className="list-disc">
             <Link
@@ -54,24 +94,7 @@ export default async function Home() {
           </li>
         </ul>
       </div>
-
-      <div className="flex flex-col items-center justify-center p-10 md:flex-row">
-        <div className="flex flex-col items-center md:flex-row">
-          <img
-            src="https://github-readme-stats.vercel.app/api?username=ShrootBuck&hide=stars&hide_title=false&hide_rank=true&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=dark&locale=en&hide_border=false&order=1"
-            alt="stats graph"
-            className="mb-5 md:mb-0 md:mr-5"
-          />
-          <img
-            src="https://github-readme-stats.vercel.app/api/top-langs?username=ShrootBuck&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=dark&hide_border=false&order=2"
-            alt="languages graph"
-          />
-        </div>
-      </div>
-
       <SocialsDock />
-
-      <br />
     </>
   );
 }
