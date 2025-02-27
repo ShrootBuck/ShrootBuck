13606acdd89c428ad126ed8741c7aedf07be3be5
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-02-26 21:48:57 -0700

    Polish location explainer

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -11,20 +11,19 @@ export default function MyThings() {
         <h1 className="pt-10 text-center text-3xl sm:text-5xl">
           <span className="inline-flex items-center gap-2">
             <LucideMapPin size={32} className="text-blue-600" />
-            Location Tracker
+            Location Tracker?
           </span>
         </h1>
 
-        <div className="m-auto text-justify lg:w-1/2">
+        <div className="m-auto lg:w-1/2">
           <h2 className="pb-10 pt-5 text-xl">
-            It&apos;s actually quite simple. Every hour, an iOS Shortcut sends
-            my iPhone&apos;s current city, state, and region to the
-            /api/location route in a POST request. The server then stores the
-            data in a PostgreSQL database (overkill) and is cached on the home
-            route for an hour to prevent unnecessary database calls.
+            It&apos;s pretty simple. Every hour, an iOS Shortcut sends my
+            iPhone&apos;s current location to the /api/location route in a POST
+            request. The server then stores the data in a PostgreSQL database
+            (which is overkill) and served to visitors.
             <br />
             <br />
-            If you&apos;re interested in the code, you can find it on my{" "}
+            If you&apos;re interested in the code, you can find it on{" "}
             <Link
               className="text-blue-600 hover:text-blue-500"
               href={"https://github.com/ShrootBuck/ShrootBuck"}
