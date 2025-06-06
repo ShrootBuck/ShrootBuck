6d301190b09e37c06fbf42248d019ed26279a338
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-06-06 17:05:10 +0300

    Remove Warden Anti-Cheat link from Other Things section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,15 +88,6 @@ export default async function Home() {
       <div className="flex justify-center pb-5">
         <ul>
           <li className="list-disc">
-            <Link
-              className="text-blue-600 hover:text-blue-500"
-              target="_blank"
-              href="https://warden.zaydkrunz.com"
-            >
-              Warden Anti-Cheat
-            </Link>
-          </li>
-          <li className="list-disc">
             <Link className="text-blue-600 hover:text-blue-500" href="/things">
               Stuff I Use
             </Link>
