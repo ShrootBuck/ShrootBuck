0a9437b0a6634c26a6827cea22a3585223397eba
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-06-12 18:38:14 +0300

    Update tools list and improve layout on things page

--- src/app/things/page.tsx
+++ src/app/things/page.tsx
@@ -25,18 +25,21 @@ const myStuffs: Stuffs[] = [
     innerstuffs: [
       { name: "Zed", url: "https://zed.dev" },
       { name: "Beekeeper Studio", url: "https://www.beekeeperstudio.io" },
-      { name: "Insomnia", url: "https://insomnia.rest" },
+      { name: "Scalar", url: "https://scalar.com" },
       { name: "Plane", url: "https://plane.so" },
       { name: "Ghostty", url: "https://ghostty.org" },
-      { name: "Docker", url: "https://www.docker.com" },
+      { name: "OrbStack", url: "https://orbstack.dev" },
     ],
   },
   {
-    header: "Productivity",
+    header: "Everyday Tools",
     innerstuffs: [
-      { name: "Proton Suite", url: "https://go.getproton.me/SH1XL" },
+      { name: "Proton Suite", url: "https://proton.me" },
       { name: "AFFiNE", url: "https://affine.pro" },
       { name: "Brave", url: "https://brave.com" },
+      { name: "TomatoBar", url: "https://github.com/AuroraWright/TomatoBar" },
+      { name: "Cobalt", url: "https://cobalt.tools" },
+      { name: "Raycast", url: "https://raycast.com" }, // Until macOS 26 lol
     ],
   },
 ];
@@ -55,40 +58,42 @@ export default function MyThings() {
       <div className="p-5">
         <Link href="/">← Back</Link>
       </div>
-      <div className="container mx-auto px-4">
+      <div className="mx-auto max-w-4xl px-4">
         <h1 className="pt-10 text-center text-3xl sm:text-5xl">My Things</h1>
         <h2 className="pb-10 pt-5 text-center text-xl">
-          The stuff I use for software development and just day-to-day life
+          The stuff I use for programming (or just other work)
         </h2>
 
-        {sortedAlphabeticallyStuffs.map((section, index) => (
-          <div key={index} className="mb-8">
-            <div className="justify-center">
-              <h2 className="mb-4 text-center text-2xl font-semibold">
-                {section.header}
-              </h2>
-              <div className="text-center">
-                <ul className="inline-block space-y-2 text-left">
-                  {section.innerstuffs.map((item, itemIndex) => (
-                    <li key={itemIndex} className="list-disc">
-                      {item.url ? (
-                        <Link
-                          href={item.url}
-                          className="text-blue-600 hover:text-blue-500 hover:underline"
-                          target="_blank"
-                        >
-                          {item.name}
-                        </Link>
-                      ) : (
-                        <p>{item.name}</p>
-                      )}
-                    </li>
-                  ))}
-                </ul>
+        <div className="flex flex-col md:flex-row md:space-x-8">
+          {sortedAlphabeticallyStuffs.map((section, index) => (
+            <div key={index} className="mb-8 flex-1">
+              <div>
+                <h2 className="mb-4 text-center text-2xl font-semibold">
+                  {section.header}
+                </h2>
+                <div className="flex justify-center">
+                  <ul className="w-full max-w-md space-y-2 text-left">
+                    {section.innerstuffs.map((item, itemIndex) => (
+                      <li key={itemIndex} className="ml-6 list-disc">
+                        {item.url ? (
+                          <Link
+                            href={item.url}
+                            className="text-blue-600 hover:text-blue-500 hover:underline"
+                            target="_blank"
+                          >
+                            {item.name}
+                          </Link>
+                        ) : (
+                          <p>{item.name}</p>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
